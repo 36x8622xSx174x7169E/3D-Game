53860a1546253d135058a8de8224ea17a665cffa
Pears0nt <159852283+36x8622xSx174x7169E@users.noreply.github.com>
2024-08-15 13:04:22 +1200

    new movement WASD

--- Player.gd
+++ Player.gd
@@ -1,12 +1,14 @@
 extends RigidBody3D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+# Called when the node enters the scene tree for the first time.# Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	var input =Input.get_action_strength("ui_up")
-	apply_central_force(input * Vector3.Foward * 1200.0 * delta)
+func _process(delta: float) -> void:
+	var input := Vector3.ZERO
+	input.x = Input.get_axis("move_left", "move_right")
+	input.z = Input.get_axis("move_forward", "move_back")
+	
+	apply_central_force(input * 1200.0 * delta)
+	
