e9dc2f741ecf0e12434c4a3dcb74720355d2a0c3
Pears0nt <159852283+36x8622xSx174x7169E@users.noreply.github.com>
2024-08-15 13:10:35 +1200

    basic floating jump but gravity is not fully working

--- Player.gd
+++ Player.gd
@@ -9,6 +9,6 @@ func _process(delta: float) -> void:
 	var input := Vector3.ZERO
 	input.x = Input.get_axis("move_left", "move_right")
 	input.z = Input.get_axis("move_forward", "move_back")
-	
+	input.y = Input.get_axis("move_down","move_up")
 	apply_central_force(input * 1200.0 * delta)
 	
