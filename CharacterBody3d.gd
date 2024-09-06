4b97d6943fa7849e78ff432f827acf1232ab7b70
Pears0nt <159852283+36x8622xSx174x7169E@users.noreply.github.com>
2024-09-07 10:10:51 +1200

    Full Character Controlls are done

--- CharacterBody3d.gd
+++ CharacterBody3d.gd
@@ -2,7 +2,7 @@ extends CharacterBody3D
 
 @onready var head = $Head
 @onready var camera = $Head/Camera3D
-@onready var collision_shape = $CollisionShape3D  # Assuming you have a CollisionShape3D node
+@onready var collision_shape = $CollisionShape3D
 
 const WALK_SPEED = 5.0
 const SPRINT_SPEED = 8.0
@@ -15,14 +15,22 @@ const FOV_CHANGE = 1.5
 
 var t_bob = 0.0
 var speed
-var _is_crouching : bool = false
+var crouching = false
+var crouch_transition_speed = 3.0  # The speed of transition for crouching/standing
+
+# Store the original height of the collision shape and camera position
+var original_collider_height = 2.0
+var crouched_collider_height = 1.5
+var original_camera_y = 0.0
+var crouched_camera_y = -0.5
+
+# Variable to store the current target state
+var target_collider_height = 2.0
+var target_camera_y = 0.0
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-
-func _input(event):
-	if event.is_action_pressed("ui_crouch"):
-		toggle_crouch()
+	original_collider_height = (collision_shape.shape as CapsuleShape3D).height
 
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
@@ -30,22 +38,34 @@ func _unhandled_input(event):
 		camera.rotate_x(-event.relative.y * SENSITIVITY)
 		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
+	# Toggle crouch when pressing the crouch button
+	if Input.is_action_just_pressed("ui_crouch"):
+		if is_on_floor():  # Only allow crouching if the character is on the floor
+			crouching = !crouching
+			_update_crouch()
+
+	# Smoothly transition between crouch and stand
+	_smooth_crouch_transition(delta)
+
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	# Handle jump.
+	# Handle jump and cancel crouch when jumping.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		# Cancel crouch when jumping
+		if crouching:
+			crouching = false
+			_update_crouch()
 		velocity.y = JUMP_VELOCITY
-	
+		
 	if Input.is_action_pressed("ui_sprint"):
 		speed = SPRINT_SPEED
 	else:
@@ -64,34 +84,40 @@ func _physics_process(delta):
 	else:
 		velocity.x = lerp(velocity.x, direction.x * speed, delta * 3.0)
 		velocity.z = lerp(velocity.z, direction.z * speed, delta * 3.0)
+		
+	# Head bob
+	t_bob += delta * velocity.length() * float(is_on_floor())
+	camera.transform.origin = _headbob(t_bob)
 	
-	# Head bobbing
-	if not _is_crouching:
-		t_bob += delta * velocity.length() * float(is_on_floor())
-		camera.transform.origin = _headbob(t_bob)
-	
-	# Update field of view
 	var velocity_clamped = clamp(velocity.length(), 0.5, SPRINT_SPEED * 2)
 	var target_fov = BASE_FOV + FOV_CHANGE * velocity_clamped
 	camera.fov = lerp(camera.fov, target_fov, delta * 8.0)
 	move_and_slide()
-
-func toggle_crouch():
-	if _is_crouching:
-		# Uncrouch
-		collision_shape.shape.height = 2.0  # Reset collision shape height
-		translation.y += 0.25  # Move character up
-		camera.transform.origin.y += 0.5  # Move camera up
-	else:
-		# Crouch
-		collision_shape.shape.height = 1.5  # Shrink collision shape height
-		translation.y -= 0.25  # Move character down
-		camera.transform.origin.y -= 0.5  # Move camera down
-	
-	_is_crouching = !_is_crouching
 
 func _headbob(time) -> Vector3:
 	var pos = Vector3.ZERO
 	pos.y = sin(time * BOB_FREQ) * BOB_AMP
 	pos.x = cos(time * BOB_FREQ / 2) * BOB_AMP
 	return pos
+
+func _update_crouch():
+	# Prevent crouching if the character is not on the floor
+	if not is_on_floor() and crouching:
+		crouching = false
+		return
+	
+	# Update target height and camera position based on crouch state
+	if crouching:
+		target_collider_height = crouched_collider_height
+		target_camera_y = crouched_camera_y
+	else:
+		target_collider_height = original_collider_height
+		target_camera_y = original_camera_y
+
+func _smooth_crouch_transition(delta):
+	# Smoothly transition the collision shape height
+	var shape = collision_shape.shape as CapsuleShape3D
+	shape.height = lerp(shape.height, target_collider_height, delta * crouch_transition_speed)
+	
+	# Smoothly transition the camera position
+	camera.transform.origin.y = lerp(camera.transform.origin.y, target_camera_y, delta * crouch_transition_speed)
